260875b1765346fd4c76bc36aab0519795ec7f8a
Vumirka <rugkevuhvictoria@gmail.com>
2026-02-10 00:51:04 +0200

    Refactor TestResult to show Gray code conversion
    Updated TestResult class to demonstrate conversion to Gray code and added example output.

--- src/TestResult.java
+++ src/TestResult.java
@@ -1,12 +1,20 @@
-//package test;
-
-//import domain.Exercise;
-
+/**
+ * Клас TestResult демонструє роботу методів класу Exercise.
+ * Він перетворює число у код Грея та за потреби декодує його назад у двійковий код
+ */
 public class TestResult {
 
     public static void main(String[] args) {
+        int binaryNumber = 17; // приклад числа
 
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        // Перетворення в код Грея
+        int grayCode = Exercise.Calculate(binaryNumber);
+
+        System.out.println("Binary number: " + Integer.toBinaryString(binaryNumber));
+        System.out.println("Gray code: " + Integer.toBinaryString(grayCode));
+
+        // Приклад зворотного перетворення
+        // int decodedBinary = Exercise.grayToBin(grayCode);
+        // System.out.println("Decoded binary: " + Integer.toBinaryString(decodedBinary));
     }
 }
